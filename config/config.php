1670c25229a5b0c0959ac53fb004e1a6d50c9117
muhbase <67451498+takanesachio@users.noreply.github.com>
2025-07-10 09:46:03 +0700

    update config.php

--- config/config.php
+++ config/config.php
@@ -3,6 +3,6 @@
 return [
 	'site_name' => 'Sachio WA Multiple Device',
 	'header_side' => 'Sachio',
-	'footer_name' => 'MPWA',
+	'footer_name' => 'Sachio',
 	'footer_copyright' => '<span class="fs-12">Made with <span class="heart"></span> by Sachio</span>',
 ];
